9eb1f61f2e0d2d2b126aa43165b2fb3670f3ffde
Ilia Gilmijarow <gilmijar@gmail.com>
2023-11-13 00:33:17 +0100

    Add random biased toss function and some more tests, first use of hypothesis

--- customers.py
+++ customers.py
@@ -6,18 +6,24 @@ for each street there will be a 40% chance of being in a new city.
 Cities are taken from https://simplemaps.com/data/world-cities - basic dataset
 """
 
-import faker
 import json
+from random import random, seed
+import faker
+from typing import List
 
 
-def read_shortlist():
+def read_shortlist() -> List[dict]:
     with open('shortlist.json', 'r') as f:
         shortlist = json.load(f)
     return shortlist
 
 
-def pick_city(n=0):
+def pick_city(n: int = 0):
     shortlist = read_shortlist()
     city = shortlist[n]
     return city['city_ascii'], city['country']
+
+
+def toss(bias: float = 0.50) -> bool:
+    return random() < bias
 
--- test_customers.py
+++ test_customers.py
@@ -1,4 +1,5 @@
 import unittest
+from hypothesis import given, example, strategies as strats
 import customers
 
 cities = [
@@ -11,15 +12,35 @@ cities = [
 ]
 
 
-class MyTestCase(unittest.TestCase):
-    def test_pick_city_gets_a_city_with_country_from_shortlist(self):
+class TestShortlist(unittest.TestCase):
+    def setUp(self) -> None:
+        # hand made monkey patching
         self.orig = customers.read_shortlist
-        customers.read_shortlist = lambda: [{
-            'city_ascii': 'Unbingu',
-            'country': 'Lemuria'
-        }]
+        customers.read_shortlist = lambda: cities
+
+    def tearDown(self) -> None:
+        # monkey un-patching
+        customers.read_shortlist = self.orig
+
+    def test_pick_city_gets_a_city_with_country_from_shortlist(self):
         city, country = customers.pick_city()
-        self.assertTupleEqual((city, country), ('Unbingu', 'Lemuria'))
+        self.assertTupleEqual((city, country), ('Faketown', 'Fakeland'))
+
+    def test_pick_city_gets_correct_city_when_called_with_parameter(self):
+        city, country = customers.pick_city(3)
+        self.assertTupleEqual((city, country), ('Storyville', 'Fictionalonia'))
+
+
+class TestRandomToss(unittest.TestCase):
+    def setUp(self) -> None:
+        customers.seed(7)
+
+    @given(strats.floats(min_value=0, max_value=1, exclude_min=True, exclude_max=True))
+    @example(0.5)
+    def test_toss_on_average_reproduce_bias(self, bias):
+        n = 100_000
+        result = sum([int(customers.toss(bias)) for _ in range(n)])/n
+        self.assertAlmostEqual(result, bias, places=2)
 
 
 if __name__ == '__main__':
